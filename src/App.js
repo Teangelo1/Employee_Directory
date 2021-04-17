07e3e164216dcf3d44466999aceb827f95d1b84f
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-17 18:35:36 -0400

    Added code for search bar, now have to retrieve data from API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './components/Header/Header';
+import Search from './components/Search/Search'
+import index from './components/Header/style.css'
 
 function App() {
   return (
 
     <div className="App">
       <Header/>
+      <Search/>
       <header className="App-header">
+       
         
       </header>
     </div>
