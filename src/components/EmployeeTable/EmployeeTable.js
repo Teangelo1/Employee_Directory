474a2ea9d5f5782f0afabd919c4eb5f8b3b96d40
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-18 13:29:37 -0400

    fixed error with trying to have a <thead> appear as a child of a <div>

--- src/components/EmployeeTable/EmployeeTable.js
+++ src/components/EmployeeTable/EmployeeTable.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
 function EmployeeTable(props) {
-  return (
-     <thead>
-      <tr>
-        <th>
-          <img alt={props.firstName} src={props.picture} />
-        </th>
-        <td>{props.firstName}</td>
-        <td>{props.lastName}</td>
-        <td>{props.phone}</td>
-        <td>{props.email}</td>
-        <td>{props.DOB}</td>
-      </tr>
-    </thead>
-  );
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <th>
+                        <img alt={props.firstName} src={props.picture} />
+                    </th>
+                    <td>{props.firstName}</td>
+                    <td>{props.lastName}</td>
+                    <td>{props.phone}</td>
+                    <td>{props.email}</td>
+                    <td>{props.DOB}</td>
+                </tr>
+            </tbody>
+        </table>
+
+
+
+    );
 }
 
 export default EmployeeTable;
